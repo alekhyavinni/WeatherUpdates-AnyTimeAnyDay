51b7e00f4cd556cac39f7738e6acbc0d5c7848eb
bootcamp vinni <alekhyavinni12@gmail.com>
2023-06-27 11:22:41 -0400

    autocomplete feature in process

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,6 +8,10 @@ var wind = document.querySelectorAll(".wind");
 var icon =document.querySelectorAll(".icon")
 
 
+function getsearchcity(){
+const Apikey ='AIzaSyAXVaVsJMefXF9k1ntdHebl7ZchfPjdEIk'
+}
+
 function getWeatherApi(){
     var requestUrl ='https://api.openweathermap.org/data/2.5/forecast?q='+cityname.value+'&appid=51e1b785d1c53673d1ab963a4ec63b88'
     fetch(requestUrl)
@@ -17,6 +21,7 @@ function getWeatherApi(){
     .then(function(data){
         console.log(data);
         
+  
         for(let j=0;j<=4;j++){
            const i=j*8;
             date[j].innerHTML=data.list[i].dt_txt;
@@ -57,12 +62,14 @@ function getWeatherApi(){
            else{
             icon[j].src="./assets/icons/clouds.png"; 
            } 
-     
         }
-        
-    })
+        }
    
+     
+    )
 }
 
 
+
 search.addEventListener('click',getWeatherApi)
+cityname.AutoComplete(getsearchcity)
